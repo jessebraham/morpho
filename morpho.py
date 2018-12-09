52aafd0949eeed444e5ccb2e37feeed401e0ad88
Jesse Braham <jesse@beta7.io>
2018-12-09 12:03:09 -0800

    Create a simple file format enum, as well as a callable converter class

--- morpho.py
+++ morpho.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import asyncio
 import logging
+import os
+import subprocess
 import sys
 import time
+
+from enum import Enum
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
@@ -14,6 +19,41 @@ logging.basicConfig(
     format="%(asctime)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
+
+
+class FileFormat(Enum):
+    alac = "m4a"
+    flac = "flac"
+
+    @property
+    def codec(self) -> str:
+        return self.name
+
+    @property
+    def extension(self) -> str:
+        return self.value
+
+
+class Ffmpeg:
+    def __init__(self):
+        self.loop = asyncio.get_event_loop()
+
+    async def convert_file(self, path: str, convert_to: FileFormat) -> None:
+        (root, _) = os.path.splitext(path)
+        write_path = f"{root}.{convert_to.extension}"
+
+        command = ["ffmpeg", "-i", path, "-c:a", convert_to.codec, write_path]
+
+        proc = await asyncio.create_subprocess_exec(
+            *command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+        await proc.wait()
+
+    def __call__(self, path: str, convert_to: FileFormat) -> None:
+        self.loop.run_until_complete(
+            asyncio.ensure_future(self.convert_file(path, convert_to))
+        )
+        self.loop.close()
 
 
 class MorphoHandler(FileSystemEventHandler):
