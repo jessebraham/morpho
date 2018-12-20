48a73bd1649c6cf7d5a429dcdc6a7197b72ffe2c
Jesse Braham <jesse@beta7.io>
2018-12-19 18:38:46 -0800

    Enqueue file system events for further processing

--- morpho/monitor.py
+++ morpho/monitor.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
 import logging
 import time
+
+from typing import Generator
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
@@ -11,20 +14,24 @@ logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
 
 class MorphoHandler(FileSystemEventHandler):
-    def __init__(self) -> None:
+    def __init__(self, max_queue_size: int = 0) -> None:
         self.logger = logging.getLogger("morpho")
+        self.loop = asyncio.get_event_loop()
+        self.queue = asyncio.Queue(max_queue_size)  # type: asyncio.Queue
 
-    def on_created(self, event) -> None:
+    def on_created(self, event) -> Generator:
         self.logger.info(event)
+        yield from self.queue.put(event)
 
-    def on_moved(self, event) -> None:
+    def on_moved(self, event) -> Generator:
         self.logger.info(event)
+        yield from self.queue.put(event)
 
 
 class Monitor:
     def __init__(self) -> None:
-        self.observer = Observer()
         self.event_handler = MorphoHandler()
+        self.observer = Observer()
 
     def register_path(self, path: str) -> None:
         self.observer.schedule(self.event_handler, path, recursive=True)
