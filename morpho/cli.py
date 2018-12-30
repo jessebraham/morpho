915dff990d445953a27f1c0ac0af7473b8646cf4
Jesse Braham <jesse@beta7.io>
2018-12-30 15:55:19 -0800

    Refactor and simplify CLI utility, additional tests

--- morpho/cli.py
+++ morpho/cli.py
@@ -13,77 +13,74 @@ from typing import List
 from morpho.core import AudioFormat, BaseFileFormat, Ffmpeg, VideoFormat
 
 
-def ensure_path_exists(path: Path) -> None:
+def convert(path: Path, convert_to: BaseFileFormat) -> None:
     if not os.path.exists(path):
         click.secho(f"ERROR: '{path}' does not exist.", fg="red")
         sys.exit(1)
 
+    files = get_file_list(path, convert_to)
+    if not files:
+        click.secho("ERROR: No files found.", fg="red")
+        sys.exit(1)
 
-def find_media(path: Path, fmt: BaseFileFormat) -> List[Path]:
-    audio_extensions = [f.extension for f in AudioFormat]
-    video_extensions = [f.extension for f in VideoFormat]
-    extensions = [
-        f for f in audio_extensions + video_extensions if f != fmt.extension
-    ]
+    file_count_maybe_plural = pluralized_file_count(len(files))
+    click.secho(f"Found {file_count_maybe_plural}.", fg="green")
 
-    return [f for extension in extensions for f in path.rglob(f"*.{extension}")]
+    converted_files = 0
+    for f in files:
+        click.echo(f"Converting '{f}' to {convert_to.codec}.")
+        if Ffmpeg.convert(f, convert_to):
+            converted_files += 1
+        else:
+            click.secho(f"Unable to convert '{f}' to {convert_to.codec}.")
+
+    file_count_maybe_plural = pluralized_file_count(converted_files)
+    click.secho(f"Converted {file_count_maybe_plural}.", fg="green")
 
 
-def get_file_list(path: Path, fmt: BaseFileFormat) -> List[Path]:
+def get_file_list(path: Path, convert_to: BaseFileFormat) -> List[Path]:
     if os.path.isdir(path):
-        files = find_media(path, fmt)
-    elif os.path.isfile(path):
-        files = [path]
+        files = find_media(path, convert_to)
     else:
-        files = []
+        files = [path]
     return files
 
 
-def convert(path: str, fmt: BaseFileFormat) -> None:
-    p = Path(path)
-    ensure_path_exists(p)
+def find_media(path: Path, convert_to: BaseFileFormat) -> List[Path]:
+    extensions = [f.extension for f in type(convert_to) if f != convert_to]
+    return [f for extension in extensions for f in path.rglob(f"*.{extension}")]
 
-    files = get_file_list(p, fmt)
-    if not files:
-        click.secho("No files found.", fg="red")
-        return
 
+def pluralized_file_count(count: int) -> str:
     e = inflect.engine()
-    num_files = len(files)
-    file_maybe_plural = e.plural("file", num_files)
+    return f"{count} {e.plural('file', count)}"
 
-    click.secho(f"Found {num_files} {file_maybe_plural}.", fg="green")
 
-    for f in files:
-        click.echo(f"Converting '{f}' to {fmt.codec}")
-        Ffmpeg.convert(f, fmt)
-
-    click.secho(f"Converted {num_files} {file_maybe_plural}!", fg="green")
+# -----------------------------------------------------------------------------
+# Commands
 
 
 @click.group()
 def cli():
-    pass
+    ...
 
 
 @cli.command()
 @click.argument("path")
 def audio(path: str) -> None:
-    # Convert to whichever format is *not* provided.
-    fmt = (
+    convert_to = (
         AudioFormat.alac
         if AudioFormat.get(path) == AudioFormat.flac
         else AudioFormat.flac
     )
-    convert(path, fmt)
+    convert(Path(path), convert_to)
 
 
 @cli.command()
 @click.argument("path")
 def video(path: str) -> None:
-    # For the time being, *always* convert to MP4.
-    fmt = VideoFormat.mp4
-    convert(path, fmt)
+    # For the time being, always convert videos to mp4.
+    convert(Path(path), VideoFormat.mp4)
 
 
 if __name__ == "__main__":
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,22 +2,43 @@
 
 from click.testing import CliRunner
 
-from morpho.cli import cli
+from morpho.cli import cli, pluralized_file_count
 
 
-def test_cli_ensure_path_exists():
-    runner = CliRunner()
-
-    result = runner.invoke(cli, ["video", "/not/a/real/path"])
-
-    assert result.exit_code == 1
-    assert result.output == "ERROR: '/not/a/real/path' does not exist.\n"
-
-
-def test_cli_requires_path():
+def test_cli_requires_path_argument():
     runner = CliRunner()
 
     result = runner.invoke(cli, ["audio"])
 
     assert result.exit_code == 2
     assert result.output.endswith('Error: Missing argument "PATH".\n')
+
+
+def test_cli_ensure_path_exists():
+    runner = CliRunner()
+    path = "/not/a/real/path"
+
+    result = runner.invoke(cli, ["video", path])
+
+    assert result.exit_code == 1
+    assert result.output == f"ERROR: '{path}' does not exist.\n"
+
+
+def test_cli_requires_non_empty_directory(tmpdir):
+    runner = CliRunner()
+
+    result = runner.invoke(cli, ["audio", str(tmpdir)])
+
+    assert result.exit_code == 1
+    assert result.output == "ERROR: No files found.\n"
+
+
+def test_cli_pluralized_file_count():
+    file_count_maybe_plural = pluralized_file_count(0)
+    assert file_count_maybe_plural == "0 files"
+
+    file_count_maybe_plural = pluralized_file_count(1)
+    assert file_count_maybe_plural == "1 file"
+
+    file_count_maybe_plural = pluralized_file_count(2)
+    assert file_count_maybe_plural == "2 files"
